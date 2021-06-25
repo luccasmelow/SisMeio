709800885a8485ed2167b89cdba30b4e474d8503
Gabriella <gabriellagomes2312@gmail.com>
2021-06-25 00:37:51 -0400

    Atualização tela Atualizar Produto

--- AtualizarProduto.xaml.cs
+++ AtualizarProduto.xaml.cs
@@ -21,5 +21,38 @@ namespace Sismeio
         {
             InitializeComponent();
         }
+
+        private void btnSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Atualização feita com sucesso", "ATUALIZAÇÃO", MessageBoxButton.OK);
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Deseja realmente cancelar a atualização ?", "ATUALIZAR PRODUTO", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    this.Close();
+                    break;
+            }
+        }
+
+        private void btnAlterar_Click(object sender, RoutedEventArgs e)
+        {
+            ClearTextBox();
+        }
+        private void ClearTextBox()
+        {
+            txtMarca.Text = "";
+            txtQtd.Text = "";
+            txtCat.Text= "";
+            txtNum.Text = "";
+            txtEstoq.Text= "";
+            txtUnit.Text="";
+            txtFrete.Text="";
+            txtDesconto.Text = "";
+             
+        }
     }
 }
