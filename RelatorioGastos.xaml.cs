25243e8ab8213c2c3bd78cb64d4916fd3d2ecc85
Alyne Pereira de Oliveira <alynepereiradeoliveira@gmail.com>
2021-06-30 18:38:57 -0400

    Auto stash before merge of "main" and "origin/main"
    Relatório de Gastos

--- RelatorioGastos.xaml.cs
+++ RelatorioGastos.xaml.cs
@@ -13,71 +13,22 @@ using System.Windows.Shapes;
 namespace Sismeio
 {
     /// <summary>
-    /// Lógica interna para ControlarGastos.xaml
+    /// Lógica interna para RelatorioGastos.xaml
     /// </summary>
-    public partial class ControlarGastos : Window
+    public partial class RelatorioGastos : Window
     {
-
-        public ControlarGastos()
+        public RelatorioGastos()
         {
             InitializeComponent();
-            Loaded += ControlarGastos_Loaded;
         }
-
-        private void btnRelatorio_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void ControlarGastos_Loaded(object sender, RoutedEventArgs e)
-        {
-            List<Despesa> ListaGastos = new List<Despesa>();
-
-
-            ListaGastos.Add(new Despesa()
-            {
-                id= 0158,
-                descricao = "Conta de Energia",
-                valordesp = 500.97
-
-
-            });
-            ListaGastos.Add(new Despesa()
-            {
-                id = 0159,
-                descricao = "Água",
-                valordesp = 150.77
-
-
-            });
-            ListaGastos.Add(new Despesa()
-            {
-                id = 0160,
-                descricao = "Pedido Arezzo",
-                valordesp = 2000.99
-
-
-            });
-
-
-
-
-            dataGridGastos.ItemsSource = ListaGastos;
-        }
-        
         private void mnuInicial_Click(object sender, RoutedEventArgs e)
         {
-            
+
         }
 
         private void mnuRealizarVenda_Click(object sender, RoutedEventArgs e)
         {
-            
+
         }
 
         private void mnuCadastrarProduto_Click(object sender, RoutedEventArgs e)
@@ -87,7 +38,7 @@ namespace Sismeio
             vsCadastrarProduto.ShowDialog();
         }
 
-        
+
         private void mnuCadastrarCliente_Click(object sender, RoutedEventArgs e)
         {
             CadastroCliente vsCadastrarCliente = new CadastroCliente();
@@ -108,7 +59,7 @@ namespace Sismeio
 
         private void mnuConsultarFuncionario_Click(object sender, RoutedEventArgs e)
         {
-            
+
         }
 
         private void mnuConsultarCliente_Click(object sender, RoutedEventArgs e)
@@ -120,7 +71,7 @@ namespace Sismeio
 
         private void mnuConsultarVenda_Click(object sender, RoutedEventArgs e)
         {
-            
+
         }
 
         private void mnuControlarGastos_Click(object sender, RoutedEventArgs e)
@@ -143,6 +94,11 @@ namespace Sismeio
             RelatorioGastos vsRelatorioGastos = new RelatorioGastos();
 
             vsRelatorioGastos.ShowDialog();
+        }
+
+        private void btnSair_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
