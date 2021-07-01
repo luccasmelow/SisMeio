05f4e14c4d2604aa4c048d83a3efdb574eaffaf7
Rodrigo Sullivan <sullivanrdrg@gmail.com>
2021-06-30 22:29:29 -0400

    up

---
+++ RealizarVendas.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sismeio
+{
+    /// <summary>
+    /// Lógica interna para RealizarVendas.xaml
+    /// </summary>
+    public partial class RealizarVendas : Window
+    {
+        public RealizarVendas()
+        {
+            InitializeComponent();
+        }
+        private void mnuInicial_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnuRealizarVenda_Click(object sender, RoutedEventArgs e)
+        {
+            RealizarVendas vsRealizarVendas = new RealizarVendas();
+
+            vsRealizarVendas.ShowDialog();
+        }
+
+        private void mnuCadastrarProduto_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroProduto vsCadastrarProduto = new CadastroProduto();
+
+            vsCadastrarProduto.ShowDialog();
+        }
+
+
+        private void mnuCadastrarCliente_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroCliente vsCadastrarCliente = new CadastroCliente();
+
+            vsCadastrarCliente.ShowDialog();
+        }
+
+        private void mnuCadastrarCompra_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void mnuConsultarEstoque_Click(object sender, RoutedEventArgs e)
+        {
+            ConsultarEstoque vsConsultarEstoque = new ConsultarEstoque();
+
+            vsConsultarEstoque.ShowDialog();
+        }
+
+        private void mnuConsultarFuncionario_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnuConsultarCliente_Click(object sender, RoutedEventArgs e)
+        {
+            ConsultaCliente vsConsultaCliente = new ConsultaCliente();
+
+            vsConsultaCliente.ShowDialog();
+        }
+
+        private void mnuConsultarVenda_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnuControlarGastos_Click(object sender, RoutedEventArgs e)
+        {
+            ControlarGastos vsControlarGastos = new ControlarGastos();
+
+            vsControlarGastos.ShowDialog();
+        }
+
+        private void mnuCadastrarFuncionario_Click(object sender, RoutedEventArgs e)
+        {
+            CadastrarFuncionario vsCadastrarFuncionario = new CadastrarFuncionario();
+
+            vsCadastrarFuncionario.ShowDialog();
+
+        }
+    }
+}
