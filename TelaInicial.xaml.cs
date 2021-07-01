1e2c55d25f501e76a3f1534a69f28badd4261397
THIAGUERA <thiagomarim2924@gmail.com>
2021-07-01 01:41:55 -0300

    1.3

--- TelaInicial.xaml.cs
+++ TelaInicial.xaml.cs
@@ -21,5 +21,54 @@ namespace Sismeio
         {
             InitializeComponent();
         }
+
+        private void btnEntrarClientes_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new ConsultaCliente2();
+
+            window.Owner = this;
+            window.ShowDialog();
+        }
+
+        private void btnProdutos_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new ConsultarEstoque();
+
+            window.Owner = this;
+            window.ShowDialog();
+        }
+
+        private void btnGestaoDeGastos_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new ControlarGastos();
+
+            window.Owner = this;
+            window.ShowDialog();
+        }
+
+        private void btnFuncionarios_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new CadastrarFuncionario();
+
+            window.Owner = this;
+            window.ShowDialog();
+        }
+
+        private void btnNovaVenda_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new RealizarVendas();
+
+            window.Owner = this;
+            window.ShowDialog();
+        }
+
+        private void btnClientes_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new ConsultaCliente2();
+
+            window.Owner = this;
+            window.ShowDialog();
+        }
     }
+    
 }
