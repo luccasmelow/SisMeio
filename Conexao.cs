d75ab2278960b611e16124baf11b38119c89f997
Alyne Pereira de Oliveira <alynepereiradeoliveira@gmail.com>
2021-10-13 16:41:16 -0400

    Conexão

---
+++ Conexao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace Sismeio
+{
+    class Conexao
+    {
+        private static string host = "localhost";
+
+        private static string port = "3306";
+
+        private static string user = "root";
+
+        private static string dbname = "bd_equipe_snow";
+
+        private static MySqlConnection conection;
+
+        private static MySqlCommand comand;
+
+
+    }
+}
