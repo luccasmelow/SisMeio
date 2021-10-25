7b789438a0a57cb1e14bf8a675072798b5b5e293
Alyne Pereira de Oliveira <alynepereiradeoliveira@gmail.com>
2021-10-25 14:23:58 -0400

    Conexao

--- Base/Conexao.cs
+++ Base/Conexao.cs
@@ -1,42 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using MySql.Data.MySqlClient;
-using System.Data;
-
-namespace Sismeio.Base
-{
-    class Conexao
-    {
-        private static string host = "localhost";
-
-        private static string port = "3306";
-
-        private static string user = "root";
-
-        private static string password = "root";
-
-        private static string dbname = "bd_equipe_snow";
-
-        private static MySqlConnection conection;
-
-        private static MySqlCommand comand;
-
-
-        public Conexao()
-        {
-            try
-            {
-                conection = new MySqlConnection($"server={host};user={user};database={dbname};port={port}; password={password}");
-                conection.Open();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-=======
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Data;
@@ -80,28 +42,12 @@ namespace Sismeio.Base
 
 
 
->>>>>>> main
         }
 
         public MySqlCommand Query()
         {
             try
             {
-<<<<<<< HEAD
-                comand = conection.CreateCommand();
-                comand.CommandType = CommandType.Text;
-
-                return comand;
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-        public void Close()
-        {
-            conection.Close();
-=======
                 command = connection.CreateCommand();
                 command.CommandType = CommandType.Text;
 
@@ -119,16 +65,10 @@ namespace Sismeio.Base
         public void Close()
         {
             connection.Close();
->>>>>>> main
         }
 
 
 
 
     }
-<<<<<<< HEAD
 }
-
-=======
-}
->>>>>>> main
