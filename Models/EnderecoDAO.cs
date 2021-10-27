579feaf5993fc896358b691db82e64b387c47fbe
Hemilly <hemillypriscilaopo@gmail.com>
2021-10-26 23:22:43 -0400

    att

---
+++ Models/EnderecoDAO.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sismeio.Base;
+
+namespace Sismeio.Models
+{
+    class EnderecoDAO
+    {
+        private static Conexao conec = new Conexao();
+        public long Insert(Endereco t)
+        {
+            try
+            {
+                var query = conec.Query();
+                query.CommandText = "INSERT INTO endereco (logradouro, numero, bairro, cidade, estado) " +
+                    "VALUES (@logradouro, @numero, @bairro, @cidade, @estado)";
+
+                query.Parameters.AddWithValue("@rua", t.Logradouro);
+                query.Parameters.AddWithValue("@numero", t.Numero);
+                query.Parameters.AddWithValue("@bairro", t.Bairro);
+                query.Parameters.AddWithValue("@cidade", t.Cidade);
+                query.Parameters.AddWithValue("@estado", t.Estado);
+
+                var result = query.ExecuteNonQuery();
+
+                if (result == 0)
+                    throw new Exception("Erro ao cadastrar o endereço. Tente Novamente!");
+
+                return query.LastInsertedId;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void Update(Endereco t)
+        {
+            try
+            {
+                var query = conec.Query();
+                query.CommandText = "UPDATE endereco SET logradouro = @logradouro, numero = @numero, bairro = @bairro, " +
+                            "cidade = @cidade, estado = @estado  WHERE cod_end = @codigo ";
+
+                query.Parameters.AddWithValue("@rua", t.Logradouro);
+                query.Parameters.AddWithValue("@numero", t.Numero);
+                query.Parameters.AddWithValue("@bairro", t.Bairro);
+                query.Parameters.AddWithValue("@cidade", t.Cidade);
+                query.Parameters.AddWithValue("@estado", t.Estado);
+
+                query.Parameters.AddWithValue("@id", t.Codigo);
+
+                var result = query.ExecuteNonQuery();
+
+                if (result == 0)
+                    throw new Exception("Erro ao atualizar o endereço. Tente Novamente!");
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
--- Models/ValidacaoCliente.cs
+++ Models/ValidacaoCliente.cs
@@ -11,7 +11,10 @@ namespace Sismeio.Models
         {
             RuleFor(x => x.Nome).NotEmpty().WithMessage("O campo Nome é obrigatório.");
             RuleFor(x => x.CPF).NotEmpty().WithMessage("O campo CPF é obrigatório.");
-
+            RuleFor(x => x.RG).NotEmpty().WithMessage("O campo RG é obrigatório.");
+            RuleFor(x => x.Telefone).NotEmpty().WithMessage("O campo Telefone é obrigatório."); 
+            RuleFor(x => x.DataNascimento).NotEmpty().WithMessage("O campo Data Nascimento é obrigatório.");
+            
         }
     }
 }
